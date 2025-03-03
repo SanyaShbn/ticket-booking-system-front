a7662c4d84f068fa9265976a00963d04818dca66
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2025-03-03 12:03:47 +0300

    Finished configuring WbSocket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { TopBarComponent } from './shared/top-bar/top-bar.component';
 import { SideNavComponent } from './shared/side-nav/side-nav.component';
+import SockJS from 'sockjs-client';
+import { Client, Message } from '@stomp/stompjs';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +14,49 @@ import { SideNavComponent } from './shared/side-nav/side-nav.component';
   imports: [
     RouterOutlet,
     TopBarComponent,
-    SideNavComponent
+    SideNavComponent,
+    CommonModule
   ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'ticket-booking-system';
+  status: string = '';
+  private stompClient: Client | null = null;
+  private statusTimeout: any;
+
+  ngOnInit() {
+    const socket = new SockJS('http://localhost:8081/ws');
+    this.stompClient = new Client({
+      webSocketFactory: () => socket,
+    });
+
+    this.stompClient.onConnect = () => {
+      this.stompClient!.subscribe('/topic/status', (message: Message) => {
+        this.status = message.body;
+
+        this.statusTimeout = setTimeout(() => {
+          this.status = '';
+        }, 3000);
+      });
+    };
+
+    this.stompClient.activate();
+  }
+
+  ngOnDestroy() {
+    if (this.statusTimeout) {
+      clearTimeout(this.statusTimeout);
+    }
+
+    if (this.stompClient) {
+      this.stompClient.deactivate();
+    }
+  }
+
+  closeStatusMessage() {
+    this.status = '';
+    if (this.statusTimeout) {
+      clearTimeout(this.statusTimeout);
+    }
+  }
 }
